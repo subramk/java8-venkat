a7602f5941a0d10f973d6f4e632c3b55b8c57a03
Kasi Subramaniam <subramaniam.kasi@sceneric.com>
2016-04-16 10:07:07 +0100

    Added more java 8 Examples

---
+++ src/main/java/com/stl/java8/examples/BasicLamdasExample.java
@@ -0,0 +1,59 @@
+package com.stl.java8.examples;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BasicLamdasExample {
+
+	static List<String> strList = Arrays.asList("kasi","subram", "uthra");  
+	
+	public static void main(String[] args) {
+		usingForEach_Without_Lambda();
+		loop_with_Lamdas(); 
+		usingForEach_with_Consumer();
+
+	}
+
+	private static void usingForEach_Without_Lambda() {
+		System.out.println("using ");
+		strList .stream().
+				forEach( element -> System.out.println("name is "+ element));
+		
+		List<String> stocks = Arrays.asList("GOOG","AMAZ","ORCL"); 
+	
+		
+		
+		
+	}
+	
+	private static void loop_with_Lamdas(){
+		strList
+			.stream()
+			.forEach(element -> System.out.println("name is "+ element)) ; 
+	}
+
+	private static void usingForEach_with_Consumer(){
+		strList
+			.stream()
+			.forEach(e -> printValues(e)) ; 
+	}
+		
+	private static <T> void printValues(T t ){
+		System.out.println("using method 2 print  ");
+		System.out.println("using printValues...." + t);
+		strList
+		.stream()
+		// method Reference  :: used here....
+		.forEach(BasicLamdasExample::printValueMethodReference) ;  // note No lambda ( No syntax of type e -> sysout(e) .... required here ) . 
+		
+	}
+	
+	
+	//Method 
+	private static <T> void printValueMethodReference(T t ){
+		System.out.println("using Method Reference to print  ");
+		System.out.println(t + "  ");
+		
+	}
+	
+}
---
+++ src/main/java/com/stl/java8/examples/BiFunctionExample.java
@@ -0,0 +1,22 @@
+package com.stl.java8.examples;
+
+import java.util.function.BiFunction;
+
+public class BiFunctionExample {
+
+	static void main(String z[]){
+		
+		bifunctionExample(); 
+	}
+
+	private static void bifunctionExample() {
+		
+	}
+	
+	public static String testStringCombiner(BiFunction<String ,String,String> biFunc){
+		return "Hello World" ; 
+	}
+	
+	
+	
+}
